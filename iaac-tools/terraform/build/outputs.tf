1b9eb7b967039085c8fec9e478109820e8f59755
tjazerzen <tjaz.erzen@zerodays.dev>
2025-02-05 20:38:44 +0100

    feat: use digital ocean instead of hetzner

--- iaac-tools/terraform/build/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "server_ip" {
-  description = "Public IPv4 address of the server"
-  value       = hcloud_server.vm.ipv4_address
-}
-
-output "dns_record" {
-  description = "Fully qualified domain name (FQDN) of the DNS record"
-  value       = cloudflare_record.domain.hostname
-}
